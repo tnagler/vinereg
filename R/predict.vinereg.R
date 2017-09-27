f74237d0827ddeb0d540621c55228eea53d6f7fa
tnagler <thomas.nagler@tum.de>
2017-09-27 04:00:56 +0200

    adjust predict()

--- R/predict.vinereg.R
+++ R/predict.vinereg.R
@@ -29,6 +29,7 @@
 #' @export
 #'
 #' @importFrom kdevine pkde1d qkde1d
+#' @importFrom rvinecopulib rvinecop
 #'
 predict.vinereg <- function(object, newdata, alpha = 0.5, uscale = FALSE, ...) {
     if (missing(newdata))
@@ -42,14 +43,14 @@ predict.vinereg <- function(object, newdata, alpha = 0.5, uscale = FALSE, ...) {
     if (length(missing_vars) > 0)
         stop("'newdata' is missing variables '", paste(missing_vars, sep = "', '"), "'")
     x <- cctools::expand_as_numeric(newdata[, colnames(object$model_frame)[-1]])
-    u <- x <- x[, object$my.index, drop = FALSE]
+    u <- x <- x[, object$selected_vars, drop = FALSE]
     if (!uscale) {
         for (j in seq.int(ncol(x))) {
-            u[, j] <- pkde1d(x[, j], object$margins[[object$my.index[j] + 1]])
+            u[, j] <- pkde1d(x[, j], object$margins[[object$selected_vars[j] + 1]])
         }
     }
 
-    uq <- qDvine(u, alpha, DVM = object$DVM)
+    uq <- qdvine(u, alpha, vine = object$vine)
     if (!uscale) {
         if (NCOL(uq) > 1) {
             q <- apply(uq, 2, qkde1d, obj = object$margins[[1]])
@@ -70,12 +71,12 @@ predict.vinereg <- function(object, newdata, alpha = 0.5, uscale = FALSE, ...) {
     q
 }
 
-qDvine <- function(u, alpha, DVM) {
-    d <- ncol(DVM$Matrix)
+qdvine <- function(u, alpha, vine) {
+    d <- ncol(vine$matrix)
     if (ncol(u) != d - 1)
-        stop("Dimensions of u and DVM are not compatible")
+        stop("Dimensions of u and vine are not compatible")
     M <- DVineMatGen(d)
-    DVM$matrix <- M[d:1, ]
+    vine$matrix <- M[d:1, ]
 
     ## obtain diagonal entries in V matrix
     n <- nrow(u)
@@ -86,8 +87,8 @@ qDvine <- function(u, alpha, DVM) {
         for (j in (d - 1):2) {
             for (k in (d - 1):j) {
                 tmp <- cbind(V2[k + 1, j, ], V[k + 1, j + 1, ])
-                V[k, j, ]  <- hbicop(tmp, 1, DVM$pair_copulas[[d - k]][[j]])
-                V2[k, j, ] <- hbicop(tmp, 2, DVM$pair_copulas[[d - k]][[j]])
+                V[k, j, ]  <- hbicop(tmp, 1, vine$pair_copulas[[d - k]][[j]])
+                V2[k, j, ] <- hbicop(tmp, 2, vine$pair_copulas[[d - k]][[j]])
             }
         }
         tmp <- t(apply(V2, 3, diag)[-1, ])
@@ -98,7 +99,7 @@ qDvine <- function(u, alpha, DVM) {
     ## predict quantile
     uq <- sapply(alpha,
                  function(a)
-                     matrix(rvinecop(n, DVM, U = cbind(a, tmp)), ncol = d)[, 1])
+                     matrix(rvinecop(n, vine, U = cbind(a, tmp)), ncol = d)[, 1])
     if (length(alpha) > 1)
         colnames(uq) <- alpha
     uq
