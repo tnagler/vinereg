47d2f50db82343f2f8d7f92d3738f36eb6e89385
tnagler <thomas.nagler@tum.de>
2018-09-12 22:59:33 +0200

    refactor predict

--- R/predict.vinereg.R
+++ R/predict.vinereg.R
@@ -42,84 +42,120 @@
 predict.vinereg <- function(object, newdata, alpha = 0.5, uscale = FALSE, ...) {
     if (missing(newdata))
         return(fitted.vinereg(object, alpha = alpha, uscale = uscale))
+
     stopifnot(length(alpha) > 0)
     if (any(is.na(alpha)) & inherits(object$model_frame[[1]], "ordered"))
         stop("cannot predict mean for ordered response.")
 
-    # check if margins were estimated
+    # predict the conditional mean if alpha contains NA
+    if (any(is.na(alpha))) {
+        alpha <- alpha[!is.na(alpha)]  # remove NA for quantile estimation
+        if (uscale)
+            stop("predicting the mean is not meaningful with `uscale = TRUE.")
+        preds_mean <- predict_mean(object, newdata)
+    } else {
+        preds_mean <- NULL
+    }
+
+    ## computation of conditional quantiles
+    if (length(alpha) > 0) {
+        stopifnot(is.numeric(alpha), all(alpha > 0), all(alpha < 1))
+
+        ## preprocessing
+        uscale <- adjust_uscale(object, uscale)
+        object$model_frame <- object$model_frame[-1]  # remove response (unused)
+        newdata <- check_and_sort_newdata(newdata, object)
+
+        # factors must be expanded to dummy numeric variables
+        x <- cctools::expand_as_numeric(newdata)
+
+        # x must be sorted in the order of the data used for fitting
+        x <- x[, names(sort(object$selected_vars)), drop = FALSE]
+
+        # quantile prediction on u-scale
+        if (!uscale)
+            x <- to_uscale(x, object)
+        preds <- qdvine(x, alpha, vine = object$vine)
+
+        # actual predictions on original scale
+        if (!uscale)
+            preds <- to_yscale(preds, object)
+
+        if (!is.null(preds_mean))
+            preds <- cbind(preds_mean, preds)
+    } else {
+        preds <- preds_mean
+    }
+
+    preds
+}
+
+#' checks if newdata has appropriate columns and sorts according to the order
+#' used for fitting.
+#' @noRd
+check_and_sort_newdata <- function(newdata, object) {
+    newdata <- as.data.frame(newdata)
+    check_var_availability(newdata, names(object$model_frame))
+
+    # the check_x functions expect variables in newdata and model_frame in
+    # the same order
+    newdata <- newdata[names(object$model_frame)]
+    check_types(newdata, object$model_frame)
+    check_levels(newdata, object$model_frame)
+
+    newdata
+}
+
+#' checks if all *selected* covariates are in newdata.
+#' @noRd
+check_var_availability <- function(newdata, vars) {
+    vars_avail <- match(vars, colnames(newdata))
+    if (any(is.na(vars_avail))) {
+        vars_missing <- paste(vars[is.na(vars_avail)], collapse = ", ")
+        stop("'newdata' is missing variables ", vars_missing)
+    }
+}
+
+#' checks if margins were estimated.
+#' @noRd
+adjust_uscale <- function(object, uscale) {
     if (!is.null(object$margins[[1]]$u) & (!uscale)) {
         warning("no margins have been estimated, setting uscale = TRUE")
         uscale <- TRUE
     }
-
-    # check if all variables in the model are in newdata
-    if (is.matrix(newdata))
-        newdata <- as.data.frame(newdata)
-    missing_vars <- setdiff(colnames(object$model_frame)[-1], colnames(newdata))
-    if (length(missing_vars) > 0)
-        stop("'newdata' is missing variables ",
-             paste(missing_vars, collapse = ", "))
-
-    # order columns as in original data and check if types are matching
-    newdata <- newdata[, colnames(object$model_frame)[-1], drop = FALSE]
-    check_types(newdata[1, ], object$model_frame[1, -1, drop = FALSE])
-
-    # predict the mean if alpha contains NA
-    if (any(is.na(alpha))) {
-        alpha <- alpha[!is.na(alpha)]
-        preds <- predict_mean(object, newdata, uscale)
-    } else {
-        preds <- NULL
-    }
-
-    if (length(alpha) > 0) {
-        stopifnot(is.numeric(alpha))
-        stopifnot(all(alpha > 0) & all(alpha < 1))
-
-        # expand factors and make ordered variables numeric
-        x <- cctools::expand_as_numeric(newdata[colnames(object$model_frame)[-1]])
-
-        # remove unused variables
-        selected_vars <- match(object$order, colnames(x))
-        u <- x <- x[, selected_vars, drop = FALSE]
-
-        # transform to uniform scale
-        if (!uscale) {
-            for (j in seq_len(ncol(x)))
-                u[, j] <- pkde1d(x[, j], object$margins[[selected_vars[j] + 1]])
-        }
-
-        # calculate quantile on uniform scale
-        q_hat <- qdvine(u, alpha, vine = object$vine)
-
-        # transform to original scale
-        if (!uscale) {
-            q_hat <- lapply(q_hat, qkde1d, obj = object$margins[[1]])
-            if (inherits(object$model_frame[[1]], "ordered")) {
-                # when response is discrete, we need to adjust the quantiles
-                lvls <- levels(object$model_frame[[1]])
-                q_hat <- lapply(q_hat, with_levels, lvls = lvls)
-            }
-        }
-
-        ## always return as data frame
-        q_hat <- as.data.frame(q_hat)
-        names(q_hat) <- alpha
-        if (!is.null(preds)) {
-            preds <- cbind(preds, q_hat)
-            alpha <- c("mean", alpha)
-        } else {
-            preds <- q_hat
-        }
-    } else {
-        alpha <- "mean"
-    }
-
-    preds <- as.data.frame(preds)
-    names(preds) <- alpha
-    preds
+    uscale
 }
 
+#' transforms data to uniform scale with probability integral transform.
+#' @noRd
+to_uscale <- function(x, object) {
+    for (var in colnames(x))
+        x[, var] <- truncate_u(pkde1d(x[, var], object$margins[[var]]))
+    x
+}
+
+truncate_u <- function(u) {
+    pmin(pmax(u, 1e-10), 1 - 1e-10)
+}
+
+#' transforms predicted response back to orginal variable scale.
+#' @noRd
+to_yscale <- function(u, object) {
+    nms <- colnames(u)
+    u <- lapply(u, qkde1d, obj = object$margins[[1]])
+    if (inherits(object$model_frame[[1]], "ordered")) {
+        # when response is discrete, we need to adjust the quantiles
+        lvls <- levels(object$model_frame[[1]])
+        u <- lapply(u, with_levels, lvls = lvls)
+    }
+
+    u <- as.data.frame(u)
+    names(u) <- nms
+    u
+}
+
+#' checks if variable types are equal in original data and new data.
+#' @noRd
 check_types <- function(actual, expected) {
     different_type <- sapply(
         seq_along(actual),
@@ -133,6 +169,16 @@ check_types <- function(actual, expected) {
         errors <- paste(capture.output(print(errors)), collapse = "\n")
         stop("some columns have incorrect type:\n", errors, call. = FALSE)
     }
+}
+
+#' checks if factor levels are equal in original data and new data.
+#' @noRd
+check_levels <- function(actual, expected) {
+    # only check factors
+    actual   <- actual[sapply(actual, is.factor)]
+    expected <- expected[sapply(expected, is.factor)]
+    if (length(expected) == 0)
+        return(TRUE)
 
     different_levels <- sapply(
         seq_along(actual),
@@ -159,48 +205,35 @@ fitted.vinereg <- function(object, alpha = 0.5, ...) {
 }
 
 
-predict_mean <- function(object, newdata, uscale) {
-    preds <- predict.vinereg(object, newdata, alpha = 1:10/ 11, uscale)
+#' predicts the conditional mean as the average of quantiles.
+#' @noRd
+predict_mean <- function(object, newdata) {
+    preds <- predict.vinereg(object, newdata, alpha = 1:10 / 11)
     data.frame(mean = rowMeans(preds))
 }
 
+#' returns the quantile predictions as order variable with appropriate levels.
+#' @noRd
 with_levels <- function(q, lvls) {
     q <- ceiling(q)
     q <- pmax(q, 1)
     q <- pmin(q, length(lvls))
-    q <- ordered(lvls[q], levels = lvls)
-    q
+    ordered(lvls[q], levels = lvls)
 }
 
-#' @importFrom rvinecopulib inverse_rosenblatt
+#' @importFrom rvinecopulib rosenblatt inverse_rosenblatt
 qdvine <- function(u, alpha, vine) {
     d <- dim(vine)[1]
     if (ncol(u) != d - 1)
         stop("Dimensions of u and vine are not compatible")
-    vine$structure <- as_rvine_structure(gen_dvine_mat(d))
 
-    ## obtain diagonal entries in V matrix
-    n <- nrow(u)
-    V <- array(NA, dim = c(d, d, n))
-    V[d, -1, ] <- t(u)
-    V2 <- V
-    if (d > 2) {
-        for (j in (d - 1):2) {
-            for (k in (d - 1):j) {
-                tmp <- cbind(V2[k + 1, j, ], V[k + 1, j + 1, ])
-                V[k, j, ]  <- hbicop(tmp, 1, vine$pair_copulas[[d - k]][[j]])
-                V2[k, j, ] <- hbicop(tmp, 2, vine$pair_copulas[[d - k]][[j]])
-            }
-        }
-        tmp <- t(apply(V2, 3, diag)[-1, ])
-    } else {
-        tmp <- V[d, 2, ]
-    }
-    tmp <- pmin(pmax(tmp, 1e-10), 1 - 1e-10)
+    cpits <- rosenblatt(cbind(0.5, u), vine)[, -1, drop = FALSE]
+    q_hat <- lapply(
+        alpha,
+        function(a) inverse_rosenblatt(cbind(a, cpits), vine)[, 1]
+    )
 
-    # return as list (will be processed further)
-    lapply(alpha,
-           function(a)
-               matrix(inverse_rosenblatt(cbind(a, tmp), vine), ncol = d)[, 1])
+    q_hat <- as.data.frame(q_hat)
+    names(q_hat) <- alpha
+    q_hat
 }
-
