27430227d422a7c0242e149d44be9a7678360b09
tnagler <thomas.nagler@tum.de>
2019-11-12 13:23:17 +0100

    simplify unit tests

--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -2,9 +2,9 @@ context("predict.vinereg()")
 
 # simulate data
 set.seed(3)
-x <- matrix(rnorm(60), 20, 3)
+x <- matrix(rnorm(30), 10, 3)
 y <- x %*% c(1, -1, 2)
-dat <- data.frame(y = y, x = x, z = as.factor(rbinom(20, 3, 0.5)))
+dat <- data.frame(y = y, x = x, z = as.factor(rbinom(10, 3, 0.5)))
 fit <- vinereg(y ~ ., family = "gauss", dat)
 
 test_that("catches missing variables", {
@@ -14,18 +14,16 @@ test_that("catches missing variables", {
 
 test_that("catches incorrect type", {
   fit <- vinereg(y ~ ., dat[1:2])
-  dat[2] <- ordered(1:20)
+  dat[2] <- ordered(1:10)
   expect_error(predict(fit, dat[2]))
 })
 
 test_that("catches incorrect levels", {
-  fit <- vinereg(y ~ ., dat)
   levels(dat[[5]]) <- 1:50
   expect_error(predict(fit, dat))
 })
 
 test_that("handles alpha correctly", {
-  fit <- vinereg(y ~ ., dat[1:3])
   expect_equal(colnames(predict(fit, alpha = c(NA, 0.5))), c("mean", "0.5"))
   expect_equal(colnames(predict(fit, alpha = c(0.1, 0.5))), c("0.1", "0.5"))
   expect_error(predict(fit, alpha = NULL))
@@ -39,7 +37,6 @@ test_that("works in bivariate case", {
 })
 
 test_that("works with continuous response", {
-  fit <- vinereg(y ~ ., dat, selcrit = "loglik")
   expect_equal(fitted(fit), predict(fit, dat))
   expect_equal(
     cbind(fitted(fit, alpha = 0.2), fitted(fit, alpha = 0.8)),
@@ -47,9 +44,9 @@ test_that("works with continuous response", {
   )
 
   # simulate data
-  x <- matrix(rnorm(300), 100, 3)
+  x <- matrix(rnorm(150), 50, 3)
   y <- x %*% c(1, -1, 2)
-  dat <- data.frame(y = y, x = x, z = as.factor(rbinom(100, 3, 0.5)))
+  dat <- data.frame(y = y, x = x, z = as.factor(rbinom(50, 3, 0.5)))
   fit <- vinereg(y ~ ., dat, selcrit = "aic")
   expect_equal(cor(dat$y, fitted(fit)[[1]]), 1, tol = 2e-1)
 })
--- tests/testthat/test-test-cpit.R
+++ tests/testthat/test-test-cpit.R
@@ -2,24 +2,21 @@ context("cpit()")
 
 # simulate data
 set.seed(3)
-x <- matrix(rnorm(60), 20, 3)
+x <- matrix(rnorm(30), 10, 3)
 y <- x %*% c(1, -1, 2)
-dat <- data.frame(y = y, x = x, z = as.factor(rbinom(20, 3, 0.5)))
+dat <- data.frame(y = y, x = x, z = as.factor(rbinom(10, 3, 0.5)))
 fit <- vinereg(y ~ ., family = "gauss", dat)
 
 test_that("catches missing variables", {
-  fit <- vinereg(y ~ ., dat[1:3])
   expect_error(cpit(fit, dat[1:2]))
 })
 
 test_that("catches incorrect type", {
-  fit <- vinereg(y ~ ., dat[1:2])
-  dat[2] <- ordered(1:20)
-  expect_error(cpit(fit, dat[1:2]))
+  dat[2] <- ordered(1:10)
+  expect_error(cpit(fit, dat))
 })
 
 test_that("catches incorrect levels", {
-  fit <- vinereg(y ~ ., dat)
   levels(dat[[5]]) <- 1:50
   expect_error(cpit(fit, dat))
 })
@@ -31,9 +28,9 @@ test_that("works in bivariate case", {
 
 test_that("works with continuous response", {
   # simulate more data for test to work appropriately
-  x <- matrix(rnorm(600), 200, 3)
+  x <- matrix(rnorm(60), 20, 3)
   y <- x %*% c(1, -1, 2)
-  dat <- data.frame(y = y, x = x, z = as.factor(rbinom(200, 3, 0.5)))
+  dat <- data.frame(y = y, x = x, z = as.factor(rbinom(20, 3, 0.5)))
   fit <- vinereg(y ~ ., dat, selcrit = "bic")
   expect_gt(ks.test(cpit(fit, dat), "punif")$p, 0.01)
 })
