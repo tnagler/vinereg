2ce1b8267e4bf9195f6a57deb39002d2cf089c5a
tnagler <thomas.nagler@tum.de>
2019-11-27 12:41:26 +0100

    fix order of selected_vars

--- src/vinereg.cpp
+++ src/vinereg.cpp
@@ -76,10 +76,11 @@ Rcpp::List select_dvine_cpp(const Eigen::MatrixXd& data,
     auto order = tools_stl::cat({ 0 }, selected_vars);
     auto new_struct = DVineStructure(tools_stl::rank(order));
 
-    std::sort(selected_vars.begin(), selected_vars.end());
+    auto sv = selected_vars;
+    std::sort(sv.begin(), sv.end());
     auto vt = std::vector<std::string>();
     vt.push_back(var_types[0]);
-    for (auto v : selected_vars)
+    for (auto v : sv)
       vt.push_back(var_types[v]);
 
     vinecop_r = Rcpp::List::create(
